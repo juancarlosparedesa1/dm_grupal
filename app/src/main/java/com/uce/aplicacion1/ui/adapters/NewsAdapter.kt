cafa82e81b661af8c469ef4c56df3927534a048a
YaniriFlorez <ymflorezrivera@gmail.com>
2024-06-04 19:46:33 -0500

    diffutil

--- app/src/main/java/com/uce/aplicacion1/ui/adapters/NewsAdapter.kt
+++ app/src/main/java/com/uce/aplicacion1/ui/adapters/NewsAdapter.kt
@@ -9,7 +9,38 @@ import com.uce.aplicacion1.ui.entites.NewsDataUI
 import com.uce.aplicacion1.databinding.ItemTopNewsBinding
 import com.uce.aplicacion1.ui.adapters.NewsDataDiffCallback
 
-
+//class NewsAdapter(
+//    private val onClickItem: (NewsDataUI) -> Unit,
+//    private val onDeleteItem: (Int) -> Unit,
+//    private val onInsertItem: () -> Unit
+//) : ListAdapter<NewsDataUI, NewsAdapter.NewsViewHolder>(NewsDataDiffCallback()) {
+//
+//    inner class NewsViewHolder(val binding: ItemTopNewsBinding) : RecyclerView.ViewHolder(binding.root) {
+//        fun bind(data: NewsDataUI, onClickItem: (NewsDataUI) -> Unit, onDeleteItem: (Int) -> Unit) {
+//            binding.txtTitleNews.text = data.name
+//            binding.txtUrlNews.text = data.url
+//            binding.txtDescNews.text = data.description
+//            binding.imgNews.load(data.image) {
+//                placeholder(R.drawable.clarin)
+//            }
+//            binding.root.setOnClickListener {
+//                onClickItem(data)
+//            }
+//            binding.btnDelete.setOnClickListener {
+//                onDeleteItem(adapterPosition)
+//            }
+//        }
+//    }
+//
+//    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NewsViewHolder {
+//        val binding = ItemTopNewsBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+//        return NewsViewHolder(binding)
+//    }
+//
+//    override fun onBindViewHolder(holder: NewsViewHolder, position: Int) {
+//        holder.bind(getItem(position), onClickItem, onDeleteItem)
+//    }
+//}
 class NewsAdapter(
     private val onClickItem: (NewsDataUI) -> Unit,
     private val onDeleteItem: (Int) -> Unit,
